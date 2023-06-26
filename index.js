2737710e3056a72cd27ac1326c772b14cbf531c9
KirillDemyanenko <demyanenkokg@gmail.com>
2023-06-26 12:08:49 +0300

    refactor: code formatting

--- index.js
+++ index.js
@@ -1,65 +1,66 @@
 import process from 'node:process';
 import readline from 'node:readline'
-import os from 'node:os'
+import os from 'node:os';
 import * as path from "path";
 
 function SendMessage(message, color) {
-    let col
+    let col;
     switch (color) {
         case 'green':
-            col = 33
-            break
+            col = 33;
+            break;
         case 'yellow':
-            col = 93
-            break
+            col = 93;
+            break;
         default:
-            col = 31
-            break
+            col = 31;
+            break;
     }
-    console.log(`\x1b[${col}m ${message} \x1b[0m`)
+    console.log(`\x1b[${col}m ${message} \x1b[0m`);
 }
 
 function printCurrentDirectory() {
-    SendMessage(`You are currently in '${process.cwd()}'`, 'green')
+    SendMessage(`You are currently in '${process.cwd()}'`, 'green');
 }
 
 function startApp() {
-    SendMessage(`Welcome to the File Manager, ${user}!`, 'green')
-    process.chdir(userHomeDirectory)
-    printCurrentDirectory()
+    SendMessage(`Welcome to the File Manager, ${user}!`, 'green');
+    process.chdir(userHomeDirectory);
+    printCurrentDirectory();
 }
 
 function exit() {
-    SendMessage(`Thank you for using File Manager, ${user}, goodbye!`, 'yellow')
-    rl.close()
+    SendMessage(`Thank you for using File Manager, ${user}, goodbye!`, 'yellow');
+    rl.close();
 }
 
 function errorHandler(err) {
-    if (err) throw new Error('Operation failed')
+    if (err) throw new Error('Operation failed');
 }
 
 const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-const args = process.argv.slice(2)
-const user = args[0].replace('--username=', '')
-const userHomeDirectory = os.homedir()
+const args = process.argv.slice(2);
+const user = args[0].replace('--username=', '');
+const userHomeDirectory = os.homedir();
 
-startApp()
+startApp();
 
 rl.on('line', (mes) => {
     switch (mes) {
         case '.exit': {
-            exit()
-            break
+            exit();
+            break;
         }
         case 'up': {
             process.chdir(path.join(process.cwd(),'../'));
-            break
+            break;
         }
         default:
-            SendMessage('Invalid input', 'red')
+            SendMessage('Invalid input', 'red');
     }
-    printCurrentDirectory()
+    printCurrentDirectory();
 })
+
 rl.on('SIGINT', () => {
-    exit()
+    exit();
 })
